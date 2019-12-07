64d2b6f23aea22331e098d4a270059f85863ba8c
Nick <github@ngpixel.com>
2019-12-07 17:05:15 -0500

    fix: clean ubuntu user ssh keys

--- scripts/90-cleanup.sh
+++ scripts/90-cleanup.sh
@@ -13,7 +13,7 @@ rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-???????? /var/log/*.log
 rm -rf /var/lib/cloud/instances/*
 rm -rf /var/lib/cloud/instance
 
-rm -f /root/.ssh/authorized_keys /etc/ssh/*key*
+rm -f /root/.ssh/authorized_keys /etc/ssh/*key* /home/ubuntu/.ssh/authorized_keys
 
 dd if=/dev/zero of=/zerofile; sync; rm /zerofile; sync
 cat /dev/null > /var/log/lastlog; cat /dev/null > /var/log/wtmp; cat /dev/null > /var/log/auth.log
